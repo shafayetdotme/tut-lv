c9f1d71cdf9d7be1c2195cb95de8eae0e1219802
Md. Shafayet Kabir <shafayet.me@gmail.com>
2020-06-24 10:40:44 +0600

    Basic File created for Post

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
